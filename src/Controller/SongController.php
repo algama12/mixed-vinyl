828496676dd5976eede539ea93d96f4f75561bfc
Alvaro Galet Martin <alvarogaletmartin@gmail.com>
2023-04-12 10:09:50 +0200

    entendiendo los servicios de symfony con bin/console debug:autowiring

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,8 +10,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id):Response
+    #[Route('/api/songs/{id<\d+>}', methods:['GET'])]
+    public function getSong(int $id, LoggerInterface $logger):Response
     {
         // TODO query the database
         $song = [
@@ -18,6 +19,11 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning API response for song {song}',[
+            'song'=>$id,
+        ]);
+
         return $this->json($song);
     }
 }
